2ff7d59457632f8fe8d488742a5853a29376d8e0
Alejandro <alejandrochvs@hotmail.com>
2017-03-08 16:07:57 +0000

    Correction

--- public/routes/index.js
+++ public/routes/index.js
@@ -1,15 +1,11 @@
 module.exports = function(app) {
   app.route('/whoami')
-    .get(function(req, res) {
-      var ip = req.headers['x-forwarded-for'] || 
-     req.connection.remoteAddress || 
-     req.socket.remoteAddress ||
-     req.connection.socket.remoteAddress;
-     var info = {
-         'ipaddress': ip,
+    .get(function(req, res) {;
+     var reponse = {
+         'ipaddress': req.headers['x-forwarded-for'],
          'language': req.headers["accept-language"].split(',')[0],
          'software': req.headers['user-agent'].split(') ')[0].split(' (')[1]
      };
-     res.send(info);
+     res.send(reponse);
     });
 }; 
